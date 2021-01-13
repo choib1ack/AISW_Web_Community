948b1b176410b225439e9a94d693f6e74f84a00b
장하영 <hyj9829@naver.com>
2021-01-13 17:01:46 +0900

    Add Bootstrap

--- web-community/src/components/Menu.js
+++ web-community/src/components/Menu.js
@@ -1,63 +1,44 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
 import './Menu.css';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    },
-}));
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export default function Menu() {
-    const classes = useStyles();
-
     return (
-        <div className={classes.root}>
-            <Grid container spacing={3}>
-                <Grid item xs>
-                    <Paper className={classes.paper} style={{background: 'transparent', boxShadow: 'none'}}>
-                        <button className="Menu-logo">
-                            가천대학교 AI&소프트웨어학부
-                        </button>
-                    </Paper>
-                </Grid>
-                <Grid item xs={6}>
-                    <Paper className={classes.paper} style={{background: 'transparent', boxShadow: 'none'}}>
-                        <button className="Menu-button">
-                            공지사항
-                        </button>
-                        <button className="Menu-button">
-                            게시판
-                        </button>
-                        <button className="Menu-button">
-                            학과정보
-                        </button>
-                        <button className="Menu-button">
-                            채용정보
-                        </button>
-                        <button className="Menu-button">
-                            공모전/대외활동
-                        </button>
-                    </Paper>
-                </Grid>
-                <Grid item xs>
-                    <Paper className={classes.paper} style={{background: 'transparent', boxShadow: 'none'}}>
-                        <button className="Menu-button">
-                            로그인
-                        </button>
-                        <button className="Menu-button blue-button">
-                            회원가입
-                        </button>
-                    </Paper>
-                </Grid>
-            </Grid>
-        </div>
+        <Container style={{margin: '0px', marginLeft: 'auto', marginRight:'auto'}}>
+            <Row>
+                <Col xs={2}>
+                    <button className="Menu-logo">
+                        가천대학교 AI&소프트웨어학부
+                    </button>
+                </Col>
+                <Col xs={7}>
+                    <button className="Menu-button">
+                        공지사항
+                    </button>
+                    <button className="Menu-button">
+                        게시판
+                    </button>
+                    <button className="Menu-button">
+                        학과정보
+                    </button>
+                    <button className="Menu-button">
+                        채용정보
+                    </button>
+                    <button className="Menu-button">
+                        공모전/대외활동
+                    </button>
+                </Col>
+                <Col xs={3}>
+                    <button className="Menu-button">
+                        로그인
+                    </button>
+                    <button className="Menu-button blue-button">
+                        회원가입
+                    </button>
+                </Col>
+            </Row>
+        </Container>
     );
 }
