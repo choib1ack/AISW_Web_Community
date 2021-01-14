de0f7f5785e041f0857d4cb0a4b980d88d51242e
장하영 <hyj9829@naver.com>
2021-01-14 22:25:43 +0900

    Add Login

--- web-community/src/App.js
+++ web-community/src/App.js
@@ -3,11 +3,13 @@ import Menu from "./components/Menu";
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from "./components/Footer";
+import Login from "./components/Login";
 
 function App() {
     return (
         <div className="App">
             <Menu/>
+            <Login/>
             <Footer/>
         </div>
     );
