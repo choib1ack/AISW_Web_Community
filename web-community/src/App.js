e0021a2dfc067eef06b7cac34772e5a18caec04d
장하영 <hyj9829@naver.com>
2021-01-15 18:00:01 +0900

    Menu-logo 수정
    font-size

--- web-community/src/App.js
+++ web-community/src/App.js
@@ -4,12 +4,13 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from "./components/Footer";
 import Login from "./components/Login";
+import Notice from "./components/Notice";
 
 function App() {
     return (
         <div className="App">
             <Menu/>
-            <Login/>
+            <Notice/>
             <Footer/>
         </div>
     );
