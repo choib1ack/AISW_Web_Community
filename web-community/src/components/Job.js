975b4c3187cfab84c16e4c4d482b6a305a81d220
장하영 <hyj9829@naver.com>
2021-01-17 18:20:20 +0900

    Add JobCard

--- web-community/src/components/Job.js
+++ web-community/src/components/Job.js
@@ -5,23 +5,24 @@ import Button from "react-bootstrap/Button";
 import classNames from "classnames";
 import searchImage from "../icon/search.svg";
 import React from "react";
-import Form from "react-bootstrap/Form";
 import './Job.css';
+import Card from "react-bootstrap/Card";
+import placeImage from "../icon/place.svg";
+import monitorImage from "../icon/monitor.svg";
 
 export default function Job() {
     return (
-        <Container className="Notice">
-            <Row style={{marginBottom: '1rem', marginTop: '2rem'}}>
+        <Container className="Job">
+            <Row style={{marginBottom: '2rem'}}>
                 <Col className="col-align">
                     <p className={"title"} style={{marginTop: 0, marginBottom: 0}}>채용 정보</p>
                 </Col>
                 <Col>
                     <img src={searchImage} style={{float: "right", marginLeft: "10px", height: "25px"}}/>
                     <input type="text" className={"search-box"} placeholder={'검색'}/>
-                    {/*style={{background:`url(${searchImage})`, backgroundRepeat:'no-repeat'}}>*/}
                 </Col>
             </Row>
-            <Row style={{marginBottom: '1rem', marginTop: '1rem'}}>
+            <Row style={{marginBottom: '2rem'}}>
                 <Button className={classNames("select-btn", "on")}>JAVA</Button>
                 <Button className={classNames("select-btn", "off")}>Spring</Button>
                 <Button className={classNames("select-btn", "off")}>Node.js</Button>
@@ -39,6 +40,47 @@ export default function Job() {
                 <Button className={classNames("select-btn", "off")}>ios</Button>
                 <Button className={classNames("select-btn", "off")}>서버/백엔드</Button>
             </Row>
+
+            <Row className="mb-3">
+                <Col className="pl-0 pr-0">
+                    <JobCard/>
+                </Col>
+                <Col className="pl-0 pr-0">
+                    <JobCard/>
+                </Col>
+            </Row>
         </Container>
+    )
+}
+
+function JobCard() {
+    return (
+        <Card style={{width: '550px'}} className="text-left flex-row">
+            <img src={searchImage} style={{width: "150px", height: "150px"}}
+                 className="ml-3 align-self-center"/>
+
+            <Card.Body>
+                <Card.Title>커머스 서버 개발 전문가</Card.Title>
+                <Card.Subtitle className="mt-2 mb-4 text-muted">중고나라</Card.Subtitle>
+
+                <Card.Text className="mb-0">
+                    <div className="d-inline-block">
+                        <img src={placeImage} style={{width: "20px", height: "20px", marginRight: "5px"}}/>
+                        <p className="d-inline-block mr-3">
+                            서울시 강남구
+                        </p>
+                    </div>
+                    <div className="d-inline-block">
+                        <img src={monitorImage} style={{width: "16px", height: "16px", marginRight: "5px"}}/>
+                        <p className="d-inline-block">
+                            신입, 인턴
+                        </p>
+                    </div>
+                </Card.Text>
+
+                <Button size="sm" disabled="true" className={classNames("select-btn", "on")}>서버/백엔드</Button>
+                <Button size="sm" disabled="true" className={classNames("select-btn", "on")}>Spring</Button>
+            </Card.Body>
+        </Card>
     )
 }
