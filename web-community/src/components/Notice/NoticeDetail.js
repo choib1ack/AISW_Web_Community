4bd61aa7ba4c45f2bd2f814d47fa927ad3de19a9
장하영 <hyj9829@naver.com>
2021-01-22 18:25:48 +0900

    목록 버튼 페이지 전환 적용

--- web-community/src/components/Notice/NoticeDetail.js
+++ web-community/src/components/Notice/NoticeDetail.js
@@ -5,7 +5,7 @@ import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Title from "../Title";
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 export default function NoticeDetail() {
     let history = useHistory();
@@ -60,10 +60,16 @@ export default function NoticeDetail() {
 }
 
 export function ListButton() {
+    let history = useHistory();
+
+    function handleClick() {
+        history.goBack();
+    }
+
     return (
         <Row style={{marginBottom: '3rem'}}>
             <Col lg={12} md={12} sm={12}>
-                <Button className="float-left btn-sm" style={{
+                <Button onClick={handleClick} className="float-left btn-sm" style={{
                     backgroundColor: 'white',
                     color: 'black',
                     borderColor: '#D4D4D4',
