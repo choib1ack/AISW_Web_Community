e99ebecb712a1461dc2fc11551073edf70681512
JunHeon-Ch <chjh121@gmail.com>
2021-01-23 20:17:27 +0900

    docs: Adding comment

--- community/src/main/java/com/aisw/community/component/LoginUserAuditorAware.java
+++ community/src/main/java/com/aisw/community/component/LoginUserAuditorAware.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+// 로그인 사용자 감시
 @Component
 public class LoginUserAuditorAware implements AuditorAware<String> {
     @Override
