4b554d6424b5badf926b265db18e7da15b66aa44
JunHeon-Ch <chjh121@gmail.com>
2021-01-23 23:03:22 +0900

    feat: Implementing University API

---
+++ community/src/main/java/com/aisw/community/model/network/request/UniversityApiRequest.java
@@ -0,0 +1,40 @@
+package com.aisw.community.model.network.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UniversityApiRequest {
+
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private String attachmentFile;
+
+    // 긴급0, 상단고정1, 일반2
+    private Long status;
+
+    private Long views;
+
+    // 학교 공지 0
+    private Long level;
+
+    // 공통 0, 글로벌1,  메디컬2
+    private Long campus;
+
+    private Long noticeId;
+}
