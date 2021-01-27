d2cd5277277bf4421dd42caf1666007be05c91a0
JunHeon-Ch <chjh121@gmail.com>
2021-01-27 19:19:31 +0900

    feat: Connecting front-end

--- community/src/main/java/com/aisw/community/controller/api/CouncilApiController.java
+++ community/src/main/java/com/aisw/community/controller/api/CouncilApiController.java
@@ -4,9 +4,11 @@ import com.aisw.community.controller.CrudController;
 import com.aisw.community.model.entity.Council;
 import com.aisw.community.model.network.request.CouncilApiRequest;
 import com.aisw.community.model.network.response.CouncilApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/notice/council")
 public class CouncilApiController extends CrudController<CouncilApiRequest, CouncilApiResponse, Council> {
