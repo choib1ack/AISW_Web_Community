62bfedf84a89e787be7b88b2e3482c546651afc4
Yang, heerim <37904738+Yangheerim@users.noreply.github.com>
2021-02-01 02:39:23 +0900

    마이페이지 Component 생성

--- web-community/src/components/Home.js
+++ web-community/src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ApiTest from "./ApiTest";
+import MyPage from "./MyPage";
 
 export default function Home() {
     return(
         <div>
             <h1>Home</h1>
-            <ApiTest/>
+            {/*<ApiTest/>*/}
+            <MyPage/>
         </div>
     )
 }
