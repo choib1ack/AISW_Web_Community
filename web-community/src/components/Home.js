26c83ff29f0ac652cf195808e72331578e3d660f
Yang, heerim <37904738+Yangheerim@users.noreply.github.com>
2021-02-19 17:06:32 +0900

    Home Component UI 완성

--- web-community/src/components/Home.js
+++ web-community/src/components/Home.js
@@ -5,6 +5,11 @@ import searchImage from "../icon/search_black.png";
 import exampleBanner from "../image/banner_example1.svg"
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import JobCard from "./JobCard";
+import jobLogo from "../image/Kakao.png";
+import jobLogo2 from "../image/naver.png";
+import ContestInfoCard from "./ComponentInfoCard";
+import contestImage1 from "../image/contest1.svg";
 export default function Home() {
     return(
         <div className="Home">
@@ -36,29 +41,120 @@ export default function Home() {
             </Row>
             <Row style={{margin:"20px 0px"}}>
                 <Col lg={6} md={6} sm={6}>
-                    <div style={{border:"1px solid #E3E3E3", padding:"20px", backgroundColor:"#EFF7F9", textAlign:"left", height:"300px"}}>
-                        공지사항
+                    <div style={{border:"1px solid #E3E3E3", padding:"20px", backgroundColor:"#EFF7F9", textAlign:"left"}}>
+                        <div style={{marginBottom:"20px"}}>
+                            공지사항 <span style={{float:"right", cursor:"pointer"}}>+더보기</span>
+                        </div>
+                        <div>
+                            <p>
+                                2021-1학기 수강신청 안내 <span style={{float:"right"}}>2021-01-06</span>
+                            </p>
+                            <p>
+                                2021-1학기 수강신청 안내 <span style={{float:"right"}}>2021-01-06</span>
+                            </p>
+                            <p>
+                                2021-1학기 수강신청 안내 <span style={{float:"right"}}>2021-01-06</span>
+                            </p>
+                            <p>
+                                2021-1학기 수강신청 안내 <span style={{float:"right"}}>2021-01-06</span>
+                            </p>
+                            <p>
+                                2021-1학기 수강신청 안내 <span style={{float:"right"}}>2021-01-06</span>
+                            </p>
+                            <p>
+                                2021-1학기 수강신청 안내 <span style={{float:"right"}}>2021-01-06</span>
+                            </p>
+                        </div>
                     </div>
-
                 </Col>
                 <Col lg={6} md={6} sm={6}>
-                    <div style={{border:"1px solid #E3E3E3", padding:"20px", backgroundColor:"#EFF7F9", textAlign:"left", height:"300px"}}>
-                        게시판
+                    <div style={{border:"1px solid #E3E3E3", padding:"20px", backgroundColor:"#EFF7F9", textAlign:"left"}}>
+                        <div style={{marginBottom:"20px"}}>
+                            게시판 <span style={{float:"right", cursor:"pointer"}}>+더보기</span>
+                        </div>
+                        <div>
+                            <p>
+                                3학년 질문이 있습니다 <span style={{float:"right"}}>2021-01-06</span>
+                            </p>
+                            <p>
+                                3학년 질문이 있습니다 <span style={{float:"right"}}>2021-01-06</span>
+                            </p>
+                            <p>
+                                3학년 질문이 있습니다 <span style={{float:"right"}}>2021-01-06</span>
+                            </p>
+                            <p>
+                                3학년 질문이 있습니다 <span style={{float:"right"}}>2021-01-06</span>
+                            </p>
+                            <p>
+                                3학년 질문이 있습니다 <span style={{float:"right"}}>2021-01-06</span>
+                            </p>
+                            <p>
+                                3학년 질문이 있습니다 <span style={{float:"right"}}>2021-01-06</span>
+                            </p>
+                        </div>
                     </div>
                 </Col>
             </Row>
             <Row style={{margin:"20px 0px"}}>
                 <Col lg={12} md={12} sm={12}>
-                    <div style={{border:"1px solid #E3E3E3", padding:"20px", textAlign:"left", height:"300px"}}>
-                        채용정보
+                    <div style={{border:"1px solid #E3E3E3", padding:"20px", textAlign:"left"}}>
+                        <div style={{marginBottom:"20px"}}>
+                            채용정보 <span style={{float:"right", cursor:"pointer"}}>+더보기</span>
+                        </div>
+                        <div>
+                            <Row>
+                                <Col lg={6} md={12} sm={12}>
+                                    <JobCard image={jobLogo} title='커머스 서버 개발 전문가'
+                                             host='중고나라' place='서울시 강남구' position='신입,인턴' detail={false}/>
+                                </Col>
+                                <Col lg={6} md={12} sm={12}>
+                                    <JobCard image={jobLogo2} title='커머스 서버 개발 전문가'
+                                             host='중고나라' place='서울시 강남구' position='신입,인턴' detail={false}/>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col lg={6} md={12} sm={12}>
+                                    <JobCard image={jobLogo} title='커머스 서버 개발 전문가'
+                                             host='중고나라' place='서울시 강남구' position='신입,인턴'/>
+                                </Col>
+                                <Col lg={6} md={12} sm={12}>
+                                    <JobCard image={jobLogo2} title='커머스 서버 개발 전문가'
+                                             host='중고나라' place='서울시 강남구' position='신입,인턴'/>
+                                </Col>
+                            </Row>
+                        </div>
                     </div>
 
                 </Col>
             </Row>
             <Row style={{margin:"20px 0px"}}>
                 <Col lg={12} md={12} sm={12}>
-                    <div style={{border:"1px solid #E3E3E3", padding:"20px", textAlign:"left", height:"300px"}}>
-                        공모전/대외활동
+                    <div style={{border:"1px solid #E3E3E3", padding:"20px", textAlign:"left"}}>
+                        <div style={{marginBottom:"20px"}}>
+                            공모전/대외활동 <span style={{float:"right", cursor:"pointer"}}>+더보기</span>
+                        </div>
+                        <Row>
+                            <Col lg={3} md={4} sm={6}>
+                                <ContestInfoCard image={contestImage1}
+                                                 title='인공지능 학습용 데이터 활용 아이디어 공모전'
+                                                 host='과학기술정보통신부' date='2020-12-14(월) ~ 2021-01-07(목)'/>
+                            </Col>
+                            <Col lg={3} md={4} sm={6}>
+                                <ContestInfoCard image={contestImage1}
+                                                 title='인공지능 학습용 데이터 활용 아이디어 공모전'
+                                                 host='과학기술정보통신부' date='2020-12-14(월) ~ 2021-01-07(목)'/>
+                            </Col>
+                            <Col lg={3} md={4} sm={6}>
+                                <ContestInfoCard image={contestImage1}
+                                                 title='인공지능 학습용 데이터 활용 아이디어 공모전'
+                                                 host='과학기술정보통신부' date='2020-12-14(월) ~ 2021-01-07(목)'/>
+                            </Col>
+                            <Col lg={3} md={4} sm={6}>
+                                <ContestInfoCard image={contestImage1}
+                                                 title='인공지능 학습용 데이터 활용 아이디어 공모전'
+                                                 host='과학기술정보통신부' date='2020-12-14(월) ~ 2021-01-07(목)'/>
+                            </Col>
+                        </Row>
                     </div>
 
                 </Col>
