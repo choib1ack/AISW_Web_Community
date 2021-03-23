831cda16085a295cc48dd3c29899101dc4b7e1bf
BlackChang <dnjs7651@gmail.com>
2021-03-23 15:48:01 +0900

    Convert "User" to "Account"

--- community/src/main/java/com/aisw/community/service/QnaApiLogicService.java
+++ community/src/main/java/com/aisw/community/service/QnaApiLogicService.java
@@ -113,23 +113,8 @@ public class QnaApiLogicService extends PostService<QnaApiRequest, BoardApiRespo
                 .likes(qna.getLikes())
                 .isAnonymous(qna.getIsAnonymous())
                 .subject(qna.getSubject())
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
-                .userId(qna.getUser().getId())
-=======
-                .category(qna.getCategory())
                 .userId(qna.getAccount().getId())
->>>>>>> Stashed changes
-=======
                 .category(qna.getCategory())
-                .userId(qna.getAccount().getId())
->>>>>>> Stashed changes
-=======
-                .category(qna.getCategory())
-                .userId(qna.getAccount().getId())
->>>>>>> Stashed changes
                 .build();
 
         return qnaApiResponse;
