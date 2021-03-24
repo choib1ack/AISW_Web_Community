22a2a0430c722e16dfbc4fb4b7150ca0b41dced3
JunHeon-Ch <chjh121@gmail.com>
2021-03-24 16:30:55 +0900

    feat: Modifying User to Account

--- community/src/main/java/com/aisw/community/repository/AccountRepository.java
+++ community/src/main/java/com/aisw/community/repository/AccountRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<Account, Long> {
+public interface AccountRepository extends JpaRepository<Account, Long> {
 //    Account findByEmail(String email);
 
     Account findByUsername(String username);
