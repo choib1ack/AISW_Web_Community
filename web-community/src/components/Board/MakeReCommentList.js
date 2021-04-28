f6f519876c3fd156e3f52b670b4503fbc0ff2d84
Yang, heerim <37904738+Yangheerim@users.noreply.github.com>
2021-04-28 23:31:38 +0900

    대댓글 UI 수정

--- web-community/src/components/Board/MakeReCommentList.js
+++ web-community/src/components/Board/MakeReCommentList.js
@@ -5,8 +5,9 @@ import userImage from "../../icon/user.svg";
 import likeImage from "../../icon/like.svg";
 import Loading from "../Loading";
 import arrowImage from "../../icon/comment_replay.png";
+import likeGrayImage from "../../icon/like_gray.svg";
 
-export default function MakeReCommentList({setIsLatest, boardReCommentData}) {
+export default function MakeReCommentList({Refresh, boardReCommentData}) {
 
     const dateFormat = (s) => {
         let result = "";
@@ -14,24 +15,51 @@ export default function MakeReCommentList({setIsLatest, boardReCommentData}) {
         return result;
     }
 
-    const handleLikeClick = async (comment_id) => {
-        await axios.get("/board/comment/likes/" +comment_id)
-            .then((res) => {
-                alert("대댓글에 좋아요를 눌렀습니다");
-                setIsLatest(false);
-                console.log(res);
+    // const handleLikeClick = async (comment_id) => {
+    //     await axios.get("/board/comment/likes/" +comment_id)
+    //         .then((res) => {
+    //             alert("대댓글에 좋아요를 눌렀습니다");
+    //             // setIsLatest(false);
+    //             Refresh();
+    //             console.log(res);
+    //
+    //         }).catch(error => {
+    //             alert("에러!");
+    //             console.log(error);
+    //         })
+    // }
 
-            }).catch(error => {
-                alert("에러!");
-                console.log(error);
-            })
+    // 댓글 좋아요랑 같은 에러남
+    const handleLikeClick = async (comment_id) => {
+        // account_id는 나중에 바꿔야함
+        const data = {
+            "account_id": 1,
+            // "board_id": id
+            "comment_id": comment_id
+        }
+        await axios.post('/like/press/',
+            {
+                headers: {
+                    "Content-Type": `application/json`
+                },
+                data
+            }
+        ).then((res) => {
+            alert("댓글에 좋아요를 눌렀습니다");
+            // setIsLatest(false);
+        }).catch(error => {
+            let errorObject = JSON.parse(JSON.stringify(error));
+            console.log(errorObject);
+            alert("에러!"+errorObject);
+        })
     }
 
     const handleDeleteClick = async (comment_id) => {
         await axios.delete("/board/comment/" + comment_id)
             .then((res) => {
                 alert("댓글을 지웠습니다");
-                setIsLatest(false);
+                // setIsLatest(false);
+                Refresh();
                 console.log(res);
 
             }).catch(error => {
@@ -54,9 +82,13 @@ export default function MakeReCommentList({setIsLatest, boardReCommentData}) {
                     <img src={userImage} style={{height: "30px"}} className="ml-3 align-self-start mt-3"/>
 
                         <Card.Body>
-                        <span style={{float: "right", fontSize: '13px', color: '#FF6262'}}>
-                        <img src={likeImage} style={{cursor: "pointer"}} onClick={()=>handleLikeClick(data.id)}/> {data.likes}
-                        </span>
+                            {data.check_like?<span style={{float: "right", fontSize: '13px', color: '#FF6262'}}>
+                                <img src={likeImage} onClick={()=>handleLikeClick(data.comment_id)}
+                                     style={{cursor: "pointer"}}/> {data.likes}</span>:
+                                <span style={{float: "right", fontSize: '13px', color: '#949494'}}>
+                                <img src={likeGrayImage} onClick={()=>handleLikeClick(data.comment_id)}
+                                     style={{cursor: "pointer"}}/> {data.likes}</span>}
+
                             <Card.Title className="mb-2" style={{fontSize: '14px'}}>{data.is_anonymous ? "익명" : data.writer}
                             <span style={{color: "#8C8C8C", fontSize: '12px', marginLeft: "10px"}}>{dateFormat(data.created_at.substring(0, 16))}</span>
                             </Card.Title>
