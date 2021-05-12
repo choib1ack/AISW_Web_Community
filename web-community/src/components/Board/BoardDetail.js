2764bdf2ee5de8c1198ab5d7d4d5c7a43fa672e0
Yang, heerim <37904738+Yangheerim@users.noreply.github.com>
2021-05-12 17:22:32 +0900

    댓글 좋아요 수정

--- web-community/src/components/Board/BoardDetail.js
+++ web-community/src/components/Board/BoardDetail.js
@@ -59,7 +59,7 @@ export default function BoardDetail({match}) {
                     press: false
                 };
             default:
-                return likeState;
+                return state;
         }
     }
 
