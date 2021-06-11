0b73a12f235ff2fef962c8c5e72754a0d0908ece
Yang, heerim <37904738+Yangheerim@users.noreply.github.com>
2021-06-11 16:08:46 +0900

    관리자-유용한 사이트 관리 UI 생성

--- web-community/src/components/Title.js
+++ web-community/src/components/Title.js
@@ -17,6 +17,14 @@ function Title(props) {
             fontWeight: 'bold',
             color: '#0472FD'
         }
+    }else if (props.type === "3") {
+        style = {
+            fontSize: '14px',
+            textAlign: 'left',
+            marginTop: '3rem',
+            fontWeight: 'bold',
+            color: '#1c1c1c'
+        }
     }
     return (
         <p style={style}>{props.text}</p>
