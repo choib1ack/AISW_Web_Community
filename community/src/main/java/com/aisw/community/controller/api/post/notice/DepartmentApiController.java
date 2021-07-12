92bb7c01be1c403988474dc46bfb2192b5a29f54
JunHeon-Ch <chjh121@gmail.com>
2021-07-12 16:41:59 +0900

    feat: Implementing file upload

--- community/src/main/java/com/aisw/community/controller/api/post/notice/DepartmentApiController.java
+++ community/src/main/java/com/aisw/community/controller/api/post/notice/DepartmentApiController.java
@@ -3,6 +3,7 @@ package com.aisw.community.controller.api.post.notice;
 import com.aisw.community.controller.api.post.NoticePostController;
 import com.aisw.community.model.entity.post.notice.Department;
 import com.aisw.community.model.network.request.post.notice.DepartmentApiRequest;
+import com.aisw.community.model.network.request.post.notice.FileUploadToDepartmentDTO;
 import com.aisw.community.model.network.response.post.notice.DepartmentApiResponse;
 import com.aisw.community.model.network.response.post.notice.NoticeResponseDTO;
 import lombok.extern.slf4j.Slf4j;
@@ -12,5 +13,5 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("/notice/department")
-public class DepartmentApiController extends NoticePostController<DepartmentApiRequest, NoticeResponseDTO, DepartmentApiResponse, Department> {
+public class DepartmentApiController extends NoticePostController<DepartmentApiRequest, FileUploadToDepartmentDTO, NoticeResponseDTO, DepartmentApiResponse, Department> {
 }
