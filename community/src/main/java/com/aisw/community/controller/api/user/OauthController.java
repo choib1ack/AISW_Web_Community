906b98fb36559cb6b3d36e7558514df8374cc9b6
Sang-un <dltkddns79@gmail.com>
2021-07-22 16:26:53 +0900

    token

--- community/src/main/java/com/aisw/community/controller/api/user/OauthController.java
+++ community/src/main/java/com/aisw/community/controller/api/user/OauthController.java
@@ -1,9 +1,14 @@
 package com.aisw.community.controller.api.user;
 
+import com.aisw.community.model.entity.user.OAuthToken;
 import com.aisw.community.service.user.OauthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @CrossOrigin
@@ -13,16 +18,29 @@ import org.springframework.web.bind.annotation.*;
 public class OauthController {
 
     private final OauthService oauthService;
+    private final HttpServletResponse response;
 
-    @GetMapping(value = "/")
+    @GetMapping(value = "")
     public void socialLoginType() {
         oauthService.request();
 
     }
 
     @GetMapping(value = "/google/callback")
-    public String callback(@RequestParam(name = "code") String code) {
+    public ResponseEntity<String> callback(@RequestParam(name = "code") String code) {
         log.info(">> 소셜 로그인 API 서버로부터 받은 code :: {}", code);
-        return oauthService.requestAccessToken(code);
+        OAuthToken oAuthToken = oauthService.requestAccessToken(code);
+        Cookie cookie = new Cookie("token", oAuthToken.getIdToken());
+        cookie.setPath("/");
+        cookie.setMaxAge(60);
+
+        response.addCookie(cookie);
+        return oauthService.getUserInfo(oAuthToken.getAccessToken());
+    }
+
+    @GetMapping(value = "/signup")
+    public ResponseEntity<String> signup(@CookieValue(name = "token") Cookie token) {
+        System.out.println(token.getValue());
+        return oauthService.getUserInfo(token.getValue());
     }
 }
