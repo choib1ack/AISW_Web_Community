10b0beffcb881b0809a42405ce1a805945740a61
Yang, heerim <37904738+Yangheerim@users.noreply.github.com>
2021-08-31 14:35:37 +0900

    홈 사이트 이미지 모바일 크기 조절

--- web-community/src/components/HomeSiteImageSlide.js
+++ web-community/src/components/HomeSiteImageSlide.js
@@ -1,5 +1,6 @@
 import Carousel from "react-grid-carousel";
 import React from "react";
+// import {BrowserView, MobileView} from 'react-device-detect';
 
 function HomeSiteImageSlide({siteInfo}) {
     // image url로 바꿔야함
@@ -12,14 +13,29 @@ function HomeSiteImageSlide({siteInfo}) {
     return(
         <>
             {/*https://github.com/x3388638/react-grid-carousel*/}
-            <Carousel cols={5} rows={1} gap={20} loop>
-                {siteInfo!=null ? siteInfo.map((data, index) => (
-                    <Carousel.Item key={index}>
-                        <a href={data.link_url}><img width="100%" src={data.file_download_uri[0]} style={site_box_style}/></a>
-                    </Carousel.Item>
 
-                )):null}
-            </Carousel>
+            {/*<BrowserView>*/}
+                <Carousel cols={5} rows={1} gap={20} loop mobileBreakpoint>
+                    {siteInfo!=null ? siteInfo.map((data, index) => (
+                        <Carousel.Item key={index}>
+                            <a href={data.link_url}><img width="100%" src={data.file_download_uri[0]} style={site_box_style}/></a>
+                        </Carousel.Item>
+
+                    )):null}
+                </Carousel>
+            {/*</BrowserView>*/}
+
+            {/*<MobileView>*/}
+            {/*    <Carousel cols={3} rows={1} gap={20} loop>*/}
+            {/*        {siteInfo!=null ? siteInfo.map((data, index) => (*/}
+            {/*            <Carousel.Item key={index}>*/}
+            {/*                <a href={data.link_url}><img width="100%" src={data.file_download_uri[0]} style={site_box_style}/></a>*/}
+            {/*            </Carousel.Item>*/}
+
+            {/*        )):null}*/}
+            {/*    </Carousel>*/}
+            {/*</MobileView>*/}
+
         </>
     )
 }
