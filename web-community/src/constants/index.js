5adc2a1afad788812f2550583768acb7b8f98164
장하영 <hyj9829@naver.com>
2021-10-01 14:20:55 +0900

    feat: API 서버 주소 고정

--- web-community/src/constants/index.js
+++ web-community/src/constants/index.js
@@ -1,3 +1,6 @@
+// export const API_SERVER_URI = 'http://13.125.104.47/api';
+export const API_SERVER_URI = 'http://localhost:8080/api';
+
 export const GOOGLE_CLIENT_ID = '1051028847648-3edseaslg7hqbrgo5q2thhdag9k6q10e.apps.googleusercontent.com';
 export const GOOGLE_REDIRECT_URI = 'http://localhost:8080/auth/google/callback';
 
