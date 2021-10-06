f23b5c02377e120fb301810fbad0fe77e4a622c1
JunHeon-Ch <chjh121@gmail.com>
2021-10-06 14:27:31 +0900

    error: hadling CORS problem

--- community/src/main/java/com/aisw/community/config/CorsConfig.java
+++ community/src/main/java/com/aisw/community/config/CorsConfig.java
@@ -15,8 +15,9 @@ public class CorsConfig {
       CorsConfiguration config = new CorsConfiguration();
       config.setAllowCredentials(false);
       config.addAllowedOrigin("*"); // Access-Control-Allow-Origin  (Response에 자동으로 추가해줌)  
-      config.addAllowedHeader("*");  // Access-Control-Request-Headers  
+      config.addAllowedHeader("*");  // Access-Control-Request-Headers
       config.addAllowedMethod("*"); // Access-Control-Request-Method
+      config.addExposedHeader("*");
       
       source.registerCorsConfiguration("/**", config);
       return new CorsFilter(source);
